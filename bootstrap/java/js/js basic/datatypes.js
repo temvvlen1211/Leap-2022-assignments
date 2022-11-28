d2566866fc4defd8eab3bb114348e190e21c112d
Evening <evening@mstars-lab2-14s-iMac.local>
2022-11-28 17:49:46 +0800

    js

---
+++ bootstrap/java/js/js basic/datatypes.js
@@ -0,0 +1,52 @@
+// 5. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined,  null гэх мэт
+let message = 120;
+console.log(typeof message);
+message = false;
+console.log(typeof message);
+message = "hi";
+console.log(typeof message);
+message = null;
+console.log(message);
+
+let test;
+console.log(typeof test);
+test = null;
+console.log(test);
+
+// 6. Утга оноогоогүй хувьсагч зарлах
+// let = "";
+// console.log();
+// 7. Утга оноосон хувьсагч зарлах
+let comments = 120;
+console.log(comments);
+// 8. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+let Firstname = "temuulen",
+  lastname = "bayrsaihan",
+  maritalstatus = "single",
+  country = "Ub",
+  age = "18";
+console.log(Firstname);
+console.log(lastname);
+console.log(maritalstatus);
+console.log(country);
+console.log(age);
+// 9. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+let Firstnames = "temuulen";
+console.log(Firstnames);
+let lastnames = "bayrsaihan";
+console.log(lastnames);
+let maritalstatuss = "single";
+console.log(maritalstatuss);
+let countrys = "Ub";
+console.log(countrys);
+let ages = "18";
+console.log(ages);
+//10. MyAge, yourAge нэртэй хувьсагч зарлан эхний утгыг оноох
+let MyAge = "18",
+  yourAge = "21";
+console.log(MyAge);
+console.log(yourAge);
+// 11. Гурвалжингийн периметрийг олох
+(let = "null"), age;
+(b = "null"), (c = "null");
+console.log(a);
